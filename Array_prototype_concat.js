f873f09f08bbfbb18298c0e39bcd470bf5617c90
ISHAQUE9812 <144792550+ISHAQUE9812@users.noreply.github.com>
2024-04-29 12:03:58 +0530

    Add files via upload

---
+++ Array_prototype_concat.js
@@ -0,0 +1,9 @@
+const marvel_heros = ["thor","ironman", "spiderman"]
+const dc_hero = ["supperman", "flash", "batman"]
+const letters = ["a","b","c"];
+const number = ["1","2","3"]
+let val = [1, , 3]
+let new_val =([4, 5]);  
+const all_new_hero = [...marvel_heros, ...dc_hero, ...letters, ...number, ...val, ...new_val]
+console.log(all_new_hero)
+                        
